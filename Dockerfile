7e07603d0fcf61fccd1e51f08c93977416419f60
uuta <nightrage.metal66@gmail.com>
2023-01-29 19:51:07 +0900

    Added docker-compose.yml

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM rust:1.66.0
 
+# INFO: it makes faster cargo build
+# https://note.com/tkhm_dev/n/n439a4b4b9422
+ENV CARGO_BUILD_TARGET_DIR=/tmp/target
+
 # Create a new directory for the application
 RUN mkdir -p /usr/src/trander-rust
 WORKDIR /usr/src/trander-rust
@@ -8,11 +12,8 @@ WORKDIR /usr/src/trander-rust
 COPY . .
 
 # Build the application
-RUN cargo build --release
+RUN cargo install cargo-watch && cargo build --release
 
 # Expose the port that the application will run on
 EXPOSE 8080
-
-# Start the application
-CMD ["cargo", "run", "--release"]
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use actix_web::{
-    web,
-    App, HttpResponse, HttpServer, Responder
-};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
@@ -11,13 +8,10 @@ async fn manual_hello() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "debug");
 
-    HttpServer::new(move || {
-        App::new().route("/index", web::get().to(manual_hello))
-    })
-    // Using 127.0.0.1 or localhost here won’t work from inside docker.
-    // Ref: https://blog.logrocket.com/packaging-a-rust-web-service-using-docker/
-    .bind(("0.0.0.0", 8080))?
-    .run()
-    .await
+    HttpServer::new(move || App::new().route("/index", web::get().to(manual_hello)))
+        // Using 127.0.0.1 or localhost here won’t work from inside docker.
+        // Ref: https://blog.logrocket.com/packaging-a-rust-web-service-using-docker/
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
 }
-
