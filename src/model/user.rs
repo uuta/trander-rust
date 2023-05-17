aefc21327a8e0447d2065233c6c7ef1661ff2f33
uuta <nightrage.metal66@gmail.com>
2023-05-18 01:40:02 +0900

    Added user model

---
+++ src/model/user.rs
@@ -0,0 +1,32 @@
+use crate::schema::users;
+use chrono::NaiveDateTime;
+use diesel::{Insertable, Queryable};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Queryable, Deserialize)]
+pub struct User {
+    pub id: u64,
+    pub unique_id: Option<String>,
+    pub name: Option<String>,
+    pub email: Option<String>,
+    pub email_verified_at: Option<NaiveDateTime>,
+    pub password: Option<String>,
+    pub avatar: Option<String>,
+    pub remember_token: Option<String>,
+    pub created_at: Option<NaiveDateTime>,
+    pub updated_at: NaiveDateTime,
+    pub check_registration: bool,
+}
+
+
+#[derive(Debug, Deserialize, Insertable)]
+#[diesel(table_name = users)]
+pub struct NewUser {
+    pub id: u64,
+    pub name: Option<String>,
+    pub email: Option<String>,
+    pub email_verified_at: Option<NaiveDateTime>,
+    pub password: Option<String>,
+    pub created_at: Option<NaiveDateTime>,
+}
+
--- tests/repository/settings_test.rs
+++ tests/repository/settings_test.rs
@@ -1,15 +1,25 @@
 use super::get_test_db_pool;
-use trander_rust::schema::settings as settings_schema;
+use trander_rust::schema::{settings as settings_schema, users as users_schema};
 use chrono::NaiveDateTime;
 use diesel::prelude::*;
 use diesel::insert_into;
 use trander_rust::repository::settings;
 use trander_rust::model::setting::NewSetting;
+use trander_rust::model::user::NewUser;
 
 #[actix_rt::test]
 async fn test_get() {
     let pool = get_test_db_pool().await;
     let mut conn = pool.get().unwrap();
+
+    let new_user = NewUser {
+        id: 1,
+        name: Some("test".to_string()),
+        email: Some("aaa@test.com".to_string()),
+        email_verified_at: Some(NaiveDateTime::parse_from_str("2023-03-04 00:00:00", "%Y-%m-%d %H:%M:%S").unwrap()),
+        password: Some("test".to_string()),
+        created_at: Some(NaiveDateTime::parse_from_str("2023-03-04 00:00:00", "%Y-%m-%d %H:%M:%S").unwrap()),
+    };
 
     let new_setting = NewSetting {
         user_id: 1,
@@ -18,6 +28,11 @@ async fn test_get() {
         direction_type: 0,
         created_at: Some(NaiveDateTime::parse_from_str("2023-03-04 00:00:00", "%Y-%m-%d %H:%M:%S").unwrap()),
     };
+
+    insert_into(users_schema::table)
+        .values(&new_user)
+        .execute(&mut conn)
+        .expect("Failed to insert new setting");
 
     insert_into(settings_schema::table)
         .values(&new_setting)
