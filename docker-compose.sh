8e3f704742d03e4a57ce92e66848109639535e98
uuta <nightrage.metal66@gmail.com>
2023-06-01 23:39:31 +0900

    Added references for Docker layer caching

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Better support of Docker layer caching in Cargo
+# https://hackmd.io/jgkoQ24YRW6i0xWd73S64A
+# https://www.reddit.com/r/rust/comments/126xeyx/exploring_the_problem_of_faster_cargo_docker/
+
 source .env
 
 ./confirm-db-connection.sh $TEST_DB_SERVICE $DB_PORT $DB_USER $DB_PASS
