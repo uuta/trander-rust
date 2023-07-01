4562c3f53ebf02387684b32fcf7c15fa7b6be2c8
uuta <nightrage.metal66@gmail.com>
2023-07-01 16:14:42 +0900

    Added a mock for settings in service

--- src/handler/settings.rs
+++ src/handler/settings.rs
@@ -1,7 +1,7 @@
 use crate::db;
 use crate::error::api_error::ApiError;
 use crate::repository::settings::ImplSettingsRepository;
-use crate::service::settings;
+use crate::service::settings::{ImplSettingsService, SettingsService};
 use actix_web::web::{Data, Json, Path};
 use actix_web::{get, Responder};
 
@@ -12,7 +12,9 @@ pub async fn get(db: Data<db::DbPool>, path: Path<u64>) -> Result<impl Responder
         .get()
         .map_err(|err| ApiError::new(format!("Error getting connection: {}", err)))?;
     let repo = ImplSettingsRepository;
-    let result = settings::get(&repo, user_id_value, &mut conn)
+    let service = ImplSettingsService;
+    let result = service
+        .get(&repo, user_id_value, &mut conn)
         .map_err(|err| ApiError::new(format!("Error loading settings: {}", err)))?;
     Ok(Json(result))
 }
--- src/service/settings.rs
+++ src/service/settings.rs
@@ -1,11 +1,29 @@
 use crate::model::setting::Setting;
 use crate::repository::settings::SettingsRepository;
 use diesel::MysqlConnection;
+use mockall::automock;
 
-pub fn get<R: SettingsRepository>(
-    repo: &R,
-    user_id_value: u64,
-    conn: &mut MysqlConnection,
-) -> Result<Vec<Setting>, diesel::result::Error> {
-    repo.get(user_id_value, conn)
+#[automock]
+pub trait SettingsService<R: SettingsRepository> {
+    fn get(
+        &self,
+        repo: &R,
+        user_id_value: u64,
+        conn: &mut MysqlConnection,
+    ) -> Result<Vec<Setting>, diesel::result::Error>;
+}
+
+pub struct ImplSettingsService;
+
+// R is a type that implements the SettingsRepository trait.
+// @see https://doc.rust-jp.rs/rust-by-example-ja/generics/gen_trait.html
+impl<R: SettingsRepository> SettingsService<R> for ImplSettingsService {
+    fn get(
+        &self,
+        repo: &R,
+        user_id_value: u64,
+        conn: &mut MysqlConnection,
+    ) -> Result<Vec<Setting>, diesel::result::Error> {
+        repo.get(user_id_value, conn)
+    }
 }
