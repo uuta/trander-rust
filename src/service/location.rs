494844c82b20c012613a9f6db55e2343a910b847
uuta <nightrage.metal66@gmail.com>
2023-07-17 03:38:21 +0900

    Added new_angle.rs

--- src/service/location.rs
+++ src/service/location.rs
@@ -1,6 +1,8 @@
-use crate::service::location::new_dest::{NewDest, NewDestService};
+use crate::service::location::new_angle::{DirectionType, NewAngle};
+use crate::service::location::new_dest::NewDest;
 use mockall::automock;
 
+pub mod new_angle;
 pub mod new_dest;
 pub mod new_distance;
 
@@ -11,40 +13,74 @@ pub trait LocationService {
 
 /// lat: latitute
 /// lng: longitude
-/// angle: angle of direction
 /// distance: distance from the point (1km = 1000.0)
+/// direction_type: DirectionType (see new_angle.rs)
 pub struct ImplLocationService {
     lng: f64,
     lat: f64,
-    angle: f64,
     distance: f64,
+    direction_type: DirectionType,
+    new_angle_service: Box<dyn NewAngle>,
+    new_dest_service: Box<dyn NewDest>,
 }
 
 impl ImplLocationService {
-    pub fn new(lng: f64, lat: f64, angle: f64, distance: f64) -> Self {
+    pub fn new(
+        lng: f64,
+        lat: f64,
+        distance: f64,
+        direction_type: DirectionType,
+        new_angle_service: Box<dyn NewAngle>,
+        new_dest_service: Box<dyn NewDest>,
+    ) -> Self {
         Self {
             lng,
             lat,
-            angle,
             distance,
+            direction_type,
+            new_angle_service,
+            new_dest_service,
         }
     }
 }
 
 impl LocationService for ImplLocationService {
     fn location(&self) -> (f64, f64) {
-        let new_dest_service = NewDestService;
-        new_dest_service.new_dest(self.lng, self.lat, self.angle, self.distance)
+        let angle = self.new_angle_service.new_angle(self.direction_type);
+        self.new_dest_service
+            .new_dest(self.lng, self.lat, angle, self.distance)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::service::location::new_angle::MockNewAngle;
+    use crate::service::location::new_dest::MockNewDest;
+    use mockall::predicate::*;
 
     #[test]
     fn test_location() {
-        let location_service = ImplLocationService::new(139.767125, 35.681236, 90.0, 100000.0);
+        let mut mock_angle_service = MockNewAngle::new();
+        mock_angle_service
+            .expect_new_angle()
+            .with(eq(DirectionType::East))
+            .return_const(45.0); // Always return 45.0
+
+        let mut mock_dest_service = MockNewDest::new();
+        mock_dest_service
+            .expect_new_dest()
+            .with(eq(139.767125), eq(35.681236), eq(45.0), eq(100000.0))
+            .return_const((35.6761685462078, 140.87174397802116)); // Always return these coordinates
+
+        let location_service = ImplLocationService::new(
+            139.767125,
+            35.681236,
+            100000.0,
+            DirectionType::East,
+            Box::new(mock_angle_service),
+            Box::new(mock_dest_service),
+        );
         let (lat, lng) = location_service.location();
         assert_eq!(lat, 35.6761685462078);
         assert_eq!(lng, 140.87174397802116);
---
+++ src/service/location/new_angle.rs
@@ -0,0 +1,121 @@
+use mockall::automock;
+use rand::Rng;
+
+#[automock]
+pub trait NewAngle {
+    fn new_angle(&self, direction_type: DirectionType) -> f64;
+}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub enum DirectionType {
+    North,
+    East,
+    South,
+    West,
+}
+
+pub enum DirectionCount {
+    Single(f64, f64),
+    Double((f64, f64), (f64, f64)),
+}
+
+pub struct NewAngleService;
+
+impl NewAngle for NewAngleService {
+    fn new_angle(&self, direction_type: DirectionType) -> f64 {
+        let direction_count = direction_type.to_angle(direction_type);
+        direction_count.gen_angle()
+    }
+}
+
+impl DirectionType {
+    pub fn to_angle(&self, direction_type: DirectionType) -> DirectionCount {
+        match direction_type {
+            DirectionType::North => DirectionCount::Double((315.0, 360.0), (0.0, 45.0)),
+            DirectionType::East => DirectionCount::Single(45.0, 135.0),
+            DirectionType::South => DirectionCount::Single(135.0, 225.0),
+            DirectionType::West => DirectionCount::Single(225.0, 315.0),
+        }
+    }
+}
+
+impl DirectionCount {
+    pub fn gen_angle(&self) -> f64 {
+        let mut rng = rand::thread_rng();
+        match self {
+            DirectionCount::Single(min, max) => rng.gen_range(*min..*max),
+            DirectionCount::Double(range1, range2) => {
+                if rng.gen_bool(0.5) {
+                    rng.gen_range(range1.0..range1.1)
+                } else {
+                    rng.gen_range(range2.0..range2.1)
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_angle() {
+        let direction_types = [
+            DirectionType::North,
+            DirectionType::East,
+            DirectionType::South,
+            DirectionType::West,
+        ];
+
+        for direction_type in direction_types.iter() {
+            let service = NewAngleService;
+            let new_angle = service.new_angle(*direction_type);
+
+            match direction_type {
+                DirectionType::North => {
+                    assert!(
+                        new_angle >= 315.0 && new_angle <= 360.0
+                            || new_angle >= 0.0 && new_angle <= 45.0
+                    );
+                }
+                DirectionType::East => {
+                    assert!(new_angle >= 45.0 && new_angle <= 135.0);
+                }
+                DirectionType::South => {
+                    assert!(new_angle >= 135.0 && new_angle <= 225.0);
+                }
+                DirectionType::West => {
+                    assert!(new_angle >= 225.0 && new_angle <= 315.0);
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_gen_angle() {
+        let direction_types = [
+            DirectionType::North,
+            DirectionType::East,
+            DirectionType::South,
+            DirectionType::West,
+        ];
+
+        for direction_type in direction_types.iter() {
+            let direction_count = direction_type.to_angle(*direction_type);
+            let angle = direction_count.gen_angle();
+
+            match direction_count {
+                DirectionCount::Single(min, max) => {
+                    assert!(angle >= min && angle <= max);
+                }
+                DirectionCount::Double(range1, range2) => {
+                    assert!(
+                        (angle >= range1.0 && angle <= range1.1)
+                            || (angle >= range2.0 && angle <= range2.1)
+                    );
+                }
+            }
+        }
+    }
+}
