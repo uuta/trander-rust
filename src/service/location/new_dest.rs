bf77ebb63f7ecd273c56ac23643f32d0232b1d03
uuta <nightrage.metal66@gmail.com>
2023-07-17 15:04:18 +0900

    Optimized format() function

--- src/service/location/new_dest.rs
+++ src/service/location/new_dest.rs
@@ -40,8 +40,28 @@ impl NewDest for NewDestService {
         (self.dest.y(), self.dest.x())
     }
 
+    /// Format the new geographic coordinate as a string.
+    /// The format is `+lat +lng` if the value is positive, otherwise `-lat -lng`.
+    ///
+    /// # Returns
+    ///
+    /// * A string representing the new geographic coordinate.
     fn format(&self) -> String {
-        format!("{} {}", self.dest.y(), self.dest.x())
+        let lat = self.dest.y();
+        let lng = self.dest.x();
+
+        let lat_str = if lat >= 0.0 {
+            format!("+{}", lat)
+        } else {
+            lat.to_string()
+        };
+        let lng_str = if lng >= 0.0 {
+            format!("+{}", lng)
+        } else {
+            lng.to_string()
+        };
+
+        format!("{}{}", lat_str, lng_str)
     }
 }
 
@@ -58,5 +78,15 @@ mod tests {
         let (lat, lng) = service.get();
         assert_eq!(lat, 35.6761685462078);
         assert_eq!(lng, 140.87174397802116);
+    }
+
+    #[test]
+    fn test_format() {
+        let mut service = NewDestService {
+            dest: Point::new(0.0, 0.0),
+        };
+        service.new_dest(139.767125, 35.681236, 90.0, 100000.0);
+        let coordinate = service.format();
+        assert_eq!(coordinate, "+35.6761685462078+140.87174397802116");
     }
 }
