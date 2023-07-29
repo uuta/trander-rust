30d8535a6f1eaeaf7b6e2ed25e16aca12c049747
uuta <nightrage.metal66@gmail.com>
2023-07-29 16:50:05 +0900

    Added automock into GooglePlaceIdsRepository

--- src/repository/google_place_ids.rs
+++ src/repository/google_place_ids.rs
@@ -1,10 +1,27 @@
 use crate::model::google_place_ids::GooglePlaceIds;
 use crate::schema;
 use diesel::prelude::*;
-use diesel::MysqlConnection;
-
 use diesel::sql_types::{BigInt, Double, Nullable, Text};
+use diesel::MysqlConnection;
+use mockall::automock;
 use schema::google_place_ids::dsl::*;
+
+#[automock]
+pub trait GooglePlaceIdsRepository {
+    fn get(
+        &self,
+        place_id_value: String,
+        conn: &mut MysqlConnection,
+    ) -> Result<Vec<GooglePlaceIds>, diesel::result::Error>;
+
+    fn upsert(
+        &self,
+        conn: &mut MysqlConnection,
+        params: UpsertParams,
+    ) -> Result<usize, diesel::result::Error>;
+}
+
+pub struct ImplGooglePlaceIdsRepository;
 
 pub struct UpsertParams {
     place_id: String,
@@ -20,21 +37,24 @@ pub struct UpsertParams {
     rating_star: Option<i64>,
 }
 
-pub fn get(
-    place_id_value: String,
-    conn: &mut MysqlConnection,
-) -> Result<Vec<GooglePlaceIds>, diesel::result::Error> {
-    google_place_ids
-        .filter(place_id.eq(place_id_value))
-        .load::<GooglePlaceIds>(conn)
-}
+impl GooglePlaceIdsRepository for ImplGooglePlaceIdsRepository {
+    fn get(
+        &self,
+        place_id_value: String,
+        conn: &mut MysqlConnection,
+    ) -> Result<Vec<GooglePlaceIds>, diesel::result::Error> {
+        google_place_ids
+            .filter(place_id.eq(place_id_value))
+            .load::<GooglePlaceIds>(conn)
+    }
 
-pub fn upsert(
-    conn: &mut MysqlConnection,
-    params: UpsertParams,
-) -> Result<usize, diesel::result::Error> {
-    let sql = diesel::sql_query(
-        "
+    fn upsert(
+        &self,
+        conn: &mut MysqlConnection,
+        params: UpsertParams,
+    ) -> Result<usize, diesel::result::Error> {
+        let sql = diesel::sql_query(
+            "
         INSERT INTO google_place_ids (
             place_id, name, icon, rating, photo, vicinity,
             user_ratings_total, price_level, lat, lng, rating_star, created_at)
@@ -52,18 +72,19 @@ pub fn upsert(
             rating_star = VALUES(rating_star),
             updated_at = NOW()
     ",
-    )
-    .bind::<Text, _>(params.place_id)
-    .bind::<Text, _>(params.name)
-    .bind::<Text, _>(params.icon)
-    .bind::<Nullable<Double>, _>(params.rating)
-    .bind::<Nullable<Text>, _>(params.photo)
-    .bind::<Nullable<Text>, _>(params.vicinity)
-    .bind::<Nullable<BigInt>, _>(params.user_ratings_total)
-    .bind::<Nullable<BigInt>, _>(params.price_level)
-    .bind::<Double, _>(params.lat)
-    .bind::<Double, _>(params.lng)
-    .bind::<Nullable<BigInt>, _>(params.rating_star);
+        )
+        .bind::<Text, _>(params.place_id)
+        .bind::<Text, _>(params.name)
+        .bind::<Text, _>(params.icon)
+        .bind::<Nullable<Double>, _>(params.rating)
+        .bind::<Nullable<Text>, _>(params.photo)
+        .bind::<Nullable<Text>, _>(params.vicinity)
+        .bind::<Nullable<BigInt>, _>(params.user_ratings_total)
+        .bind::<Nullable<BigInt>, _>(params.price_level)
+        .bind::<Double, _>(params.lat)
+        .bind::<Double, _>(params.lng)
+        .bind::<Nullable<BigInt>, _>(params.rating_star);
 
-    sql.execute(conn)
+        sql.execute(conn)
+    }
 }
