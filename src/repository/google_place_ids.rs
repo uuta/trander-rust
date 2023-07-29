2d953cb50215e0b1bf781bf1a03eb31261b6f9a3
uuta <nightrage.metal66@gmail.com>
2023-07-29 16:30:50 +0900

    Implemented upsert in google_place_ids.rs

--- src/repository/google_place_ids.rs
+++ src/repository/google_place_ids.rs
@@ -3,7 +3,22 @@ use crate::schema;
 use diesel::prelude::*;
 use diesel::MysqlConnection;
 
+use diesel::sql_types::{BigInt, Double, Nullable, Text};
 use schema::google_place_ids::dsl::*;
+
+pub struct UpsertParams {
+    place_id: String,
+    name: String,
+    icon: String,
+    rating: Option<f64>,
+    photo: Option<String>,
+    vicinity: Option<String>,
+    user_ratings_total: Option<i64>,
+    price_level: Option<i64>,
+    lat: f64,
+    lng: f64,
+    rating_star: Option<i64>,
+}
 
 pub fn get(
     place_id_value: String,
@@ -12,4 +27,43 @@ pub fn get(
     google_place_ids
         .filter(place_id.eq(place_id_value))
         .load::<GooglePlaceIds>(conn)
+}
+
+pub fn upsert(
+    conn: &mut MysqlConnection,
+    params: UpsertParams,
+) -> Result<usize, diesel::result::Error> {
+    let sql = diesel::sql_query(
+        "
+        INSERT INTO google_place_ids (
+            place_id, name, icon, rating, photo, vicinity,
+            user_ratings_total, price_level, lat, lng, rating_star, created_at)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())
+        ON DUPLICATE KEY UPDATE
+            name = VALUES(name),
+            icon = VALUES(icon),
+            rating = VALUES(rating),
+            photo = VALUES(photo),
+            vicinity = VALUES(vicinity),
+            user_ratings_total = VALUES(user_ratings_total),
+            price_level = VALUES(price_level),
+            lat = VALUES(lat),
+            lng = VALUES(lng),
+            rating_star = VALUES(rating_star),
+            updated_at = NOW()
+    ",
+    )
+    .bind::<Text, _>(params.place_id)
+    .bind::<Text, _>(params.name)
+    .bind::<Text, _>(params.icon)
+    .bind::<Nullable<Double>, _>(params.rating)
+    .bind::<Nullable<Text>, _>(params.photo)
+    .bind::<Nullable<Text>, _>(params.vicinity)
+    .bind::<Nullable<BigInt>, _>(params.user_ratings_total)
+    .bind::<Nullable<BigInt>, _>(params.price_level)
+    .bind::<Double, _>(params.lat)
+    .bind::<Double, _>(params.lng)
+    .bind::<Nullable<BigInt>, _>(params.rating_star);
+
+    sql.execute(conn)
 }
