9cd1c6a63fbcc2fc555292e1d0685243730d3acf
uuta <nightrage.metal66@gmail.com>
2023-09-26 01:29:58 +0900

    Added a model of request_limits table

---
+++ src/model/request_limits.rs
@@ -0,0 +1,23 @@
+use crate::schema::request_limits;
+use chrono::NaiveDateTime;
+use diesel::{Insertable, Queryable};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Queryable, Deserialize, Clone, PartialEq)]
+pub struct RequestLimit {
+    pub id: u64,
+    pub user_id: u64,
+    pub request_limit: u64,
+    pub first_request_at: NaiveDateTime,
+    pub created_at: Option<NaiveDateTime>,
+    pub updated_at: Option<NaiveDateTime>,
+}
+
+#[derive(Debug, Deserialize, Insertable)]
+#[diesel(table_name = request_limits)]
+pub struct NewRequestLimit {
+    pub user_id: u64,
+    pub request_limit: u64,
+    pub first_request_at: NaiveDateTime,
+    pub created_at: Option<NaiveDateTime>,
+}
